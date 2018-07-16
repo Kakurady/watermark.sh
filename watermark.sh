661eb95da5316cf1685c3d0e11363be363a27e25
Kakurady <kakurady@gmail.com>
2018-07-15 21:29:34 -0400

    add skip watermarking to watermark.sh
    this helps only maintaining one set of script

--- watermark.sh
+++ watermark.sh
@@ -3,7 +3,7 @@
 
 ## constants ##
 WATERMARK=/home/kakurady/works/2017/kakurady_lettering_watermark.png
-CUSTOM_QTABLE=/home/kakurady/dev/watermark_images/qtbl_Peterson93.txt # optional
+#CUSTOM_QTABLE=/home/kakurady/dev/watermark_images/qtbl_Peterson93.txt # optional
 
 CJPEG=/home/kakurady/apps/mozjpeg/bin/cjpeg
 COMPOSITE=composite
@@ -13,10 +13,11 @@ EXIFTOOL=exiftool
 PARALLEL=parallel
 parallel_params="--bar --ungroup --load 95%"
 
-USAGE_CMDLINE="Usage: $0 [-gk] files ..."
+USAGE_CMDLINE="Usage: $0 [-gwks] files ..."
 
 USAGE="$USAGE_CMDLINE
     -g      set gravity
+    -w      skip watermarking
     -k      keep original file
     -s      report DSSIM
 "
@@ -25,13 +26,15 @@ USAGE="$USAGE_CMDLINE
 keep_original=""
 report_dssim=""
 gravity="southeast"
+do_watermark="y"
 
-while getopts g:k option_name
+while getopts g:wks option_name
 do
     case $option_name in
     g)      gravity=$OPTARG;;
     k)      keep_original="y";;
     s)      report_dssim="y";;
+    w)      do_watermark="";;
     \?)     echo "$USAGE"; exit 1;;
     esac
 done
@@ -91,7 +94,9 @@ report_ssim() {
 
 # Remember to export variables for parallel
 doIt() {
+    local watermarked_image
 	echo "working on $1"
+	
 	
 	#convert full-sized image
 	#
@@ -103,7 +108,7 @@ doIt() {
 	# ( https://www.imagemagick.org/script/command-line-options.php#quality )
 	$CONVERT -quality 3 "$1" "${1%.*}.png"
 	$CJPEG -quant-table 2 -quality 95 -outfile "${1%.*}.jpg" "${1%.*}.png"
-	rm "${1%.*}.png"
+
 	
 	#composite watermarked image
 	#
@@ -112,8 +117,16 @@ doIt() {
 	# JPEG quality factor 70 here is arbitrary
 	#
 	# since the display is a lot denser you can get away with throwing away details ("compressive image"). (however, you can't compare quality factors between encoders with different quantization tables; comparing subjective quality at a given file size is more reasonable [JPEG files don't actually have a quality factor; you're scaling a 64-entry table on how accurately details are stored in a 8x8 block])
-	$COMPOSITE -gravity $gravity -geometry +32+32 -quality 3 "$WATERMARK" "$1" "watermarked/${1%.*}_watermarked.png"
-	$CJPEG -quality 70 -quant-table 2 -outfile "watermarked/${1%.*}_large.jpg" "watermarked/${1%.*}_watermarked.png"
+	if [ "$do_watermark" ]
+	then
+	    watermarked_image="watermarked/${1%.*}_watermarked.png"
+    	$COMPOSITE -gravity $gravity -geometry +4+4 -quality 3 "$WATERMARK" "${1%.*}.png" "$watermarked_image"
+    	rm "${1%.*}.png"
+	else
+	    watermarked_image="${1%.*}.png"
+	fi
+
+	$CJPEG -quality 70 -quant-table 2 -outfile "watermarked/${1%.*}_large.jpg" "$watermarked_image"
 	
 	# shrink down watermarked image
 	#
@@ -127,13 +140,13 @@ doIt() {
 	# but I can't tell differences after looking at too many photos
 	# quant table 2 was personal pref; no discernable difference from
 	# table 0 at the same quality factor
-	$CONVERT "watermarked/${1%.*}_watermarked.png" -gamma .45455 -resize 1200x960 -gamma 2.2 -quality 3 "resized/${1%.*}_resized.png"
-	rm "watermarked/${1%.*}_watermarked.png"
+	$CONVERT "$watermarked_image" -gamma .45455 -resize 1200x960 -gamma 2.2 -quality 3 "resized/${1%.*}_resized.png"
+	rm "$watermarked_image"
 	$CJPEG -quant-table 2 -quality 92.5 -outfile "resized/${1%.*}_medium.jpg" "resized/${1%.*}_resized.png"
 	
 	#test subsampling modes
-	$CJPEG -quant-table 2 -quality 87 -sample 1x1 -outfile "resized_87_1x1/${1%.*}_1.jpg" "resized/${1%.*}_resized.png"
-	$CJPEG -quant-table 2 -quality 92.5 -sample 2x2,1x1,2x2 -outfile "resized_92_212/${1%.*}_2.jpg" "resized/${1%.*}_resized.png"
+	#$CJPEG -quant-table 2 -quality 87 -sample 1x1 -outfile "resized_87_1x1/${1%.*}_1.jpg" "resized/${1%.*}_resized.png"
+	#$CJPEG -quant-table 2 -quality 92.5 -sample 2x2,1x1,2x2 -outfile "resized_92_212/${1%.*}_2.jpg" "resized/${1%.*}_resized.png"
 	if [ -f "$CUSTOM_QTABLE" ]
 	then
 		$CJPEG -quality 90 -qtables "$CUSTOM_QTABLE" -qslots 0,1,2 -sample 1x1 -outfile "resized_92_p93/${1%.*}_p.jpg" "resized/${1%.*}_resized.png"
@@ -150,8 +163,9 @@ doIt() {
 	
 	#add exif tags
 	#
-	#May need to add color space if not sRGB. However, that bloats image
-	$EXIFTOOL -tagsFromFile "$1" --Olympus:all --Nikon:all -overwrite_original "${1%.*}.jpg" "resized/${1%.*}_medium.jpg" "watermarked/${1%.*}_large.jpg" "resized_87_1x1/${1%.*}_1.jpg"  "resized_92_212/${1%.*}_2.jpg" "resized_92_p93/${1%.*}_p.jpg" "resized_92_p87/${1%.*}_q.jpg"
+	#May need to add color space if not sRGB. However, that bloats image (by 6kb)
+	#if only we could insert the PNG built-in sRGB chunk from RawTherapee?
+	$EXIFTOOL -use MWG -tagsFromFile "$1" --Olympus:all --Nikon:all -all -icc_profile -overwrite_original -z "${1%.*}.jpg" "resized/${1%.*}_medium.jpg" "watermarked/${1%.*}_large.jpg" # "resized_87_1x1/${1%.*}_1.jpg"  "resized_92_212/${1%.*}_2.jpg" "resized_92_p93/${1%.*}_p.jpg" "resized_92_p87/${1%.*}_q.jpg"
 	
 	#move RawTherapee sidecar file, if one exist
 	if [ -f "$1.out.pp3" ]
@@ -180,6 +194,7 @@ export CUSTOM_QTABLE
 export WATERMARK
 export keep_original
 export gravity
+export do_watermark
 
 $PARALLEL $parallel_params doIt ::: "$@"
 cat resized/*.report.txt >> resized/report_summary.txt
